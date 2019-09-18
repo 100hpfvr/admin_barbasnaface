4f104993db6e2ee1efe52cc09f5993141958a1bb
mateus <mbellomo.p@gmail.com>
2019-09-18 03:37:30 -0300

    sort de pedidos pronto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:admin_barbasnaface/tabs/orders_tab.dart';
 import 'package:admin_barbasnaface/tabs/users_tab.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -81,6 +82,39 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: _buildFloating(),
     );
+  }
+
+  Widget _buildFloating() {
+    switch (_page) {
+      case 0:
+        return null;
+      case 1:
+        return SpeedDial(
+          child: Icon(Icons.sort, color: Colors.black,),
+          backgroundColor: Colors.amber,
+          overlayOpacity: 0.8,
+          overlayColor: Colors.black,
+          children: [
+            SpeedDialChild(
+                child: Icon(Icons.arrow_downward, color: Colors.amber),
+                backgroundColor: Colors.black,
+                label: "Concluídos Abaio",
+                labelStyle: TextStyle(fontSize: 14),
+                onTap: () {
+                  _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+                }),
+            SpeedDialChild(
+                child: Icon(Icons.arrow_upward, color: Colors.amber),
+                backgroundColor: Colors.black,
+                label: "Concluídos Acima",
+                labelStyle: TextStyle(fontSize: 14),
+                onTap: () {
+                  _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+                })
+          ],
+        );
+    }
   }
 }
