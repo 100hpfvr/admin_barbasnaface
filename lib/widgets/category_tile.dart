08b2916041be7844a38a9fda9f95d30bb632a96a
mateus <mbellomo.p@gmail.com>
2019-09-22 10:20:46 -0300

    tab de pedidos pronta

--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -16,6 +16,29 @@ class CategoryTile extends StatelessWidget {
               category.data["title"],
               style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
             ),
+            children: <Widget>[
+              FutureBuilder<QuerySnapshot>(
+                  future: category.reference.collection("Itens").getDocuments(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return Container();
+                    return Column(
+                        children: snapshot.data.documents.map((doc) {
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  doc.data["images"][0]),
+                            ),
+                            title: Text(doc.data["title"]),
+                            trailing:
+                            Text("R\$${doc.data["price"].toStringAsFixed(2)}"),
+                            onTap:(){
+
+
+                            },
+                          );
+                        }).toList());
+                  })
+            ],
             leading: CircleAvatar(
               backgroundImage: NetworkImage(category.data["icon"]),
             ),
